ea3e2ff386605833d2bf7b82f8c6ad0c4eede2c6
MahhhIcribk <143530431+MahhhIcribk@users.noreply.github.com>
2026-08-04 20:08:44 -0300

    nno

--- api.php
+++ api.php
@@ -1,2 +1,21 @@
 // Transforme o array do PHP em JSON puro, o que caralhos é isso
+echo jason_encode($vendas, JSON_PRETTY_PRINT)
+// SLQ  
+
+// PHP => API
+
+// Organização de pastas na qual se deve nomear mmas quando chama em alguns codigo , deve chamar a pagina pra dai chamar o arquivo
+
+pastas nomes
+/includes
+/pages = paginas criar as paginas
+todo projto bem elaborado vaiter um index na raiz, pra que? pra ser o inicial e o direcionador
+champ
+.ts = type script = funçao pra que
+.json funçao pra que
+SRC = oque pra se utiliza  geralmente conponhe todo o codigo de verdade
+tsconfig.json = nnome de umm arquivo, qual a função 
+dash board = se nao escreevi errado
+// SRC => ts
+// 04+08+26
 
